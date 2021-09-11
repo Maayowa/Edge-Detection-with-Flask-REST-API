da81f73d7c1ebb5d082bdd0fe1402cd628c5e912
unknown <iwakinmayowa@gmail.com>
2021-09-11 21:31:17 +0100

    BWcartoonize endpoint added. Error page next

--- Torch-Flask-ImageCartoonization/app.py
+++ Torch-Flask-ImageCartoonization/app.py
@@ -11,15 +11,19 @@ Check https://github.com/SystemErrorWang/FacialCartoonization to read about the 
 from PIL import Image
 import cv2 as cv
 import os
+import base64
 import io
+from flask.json import jsonify
 import numpy as np
-from flask import Flask, send_file, Response, request
+from flask import Flask, send_file, Response, request, render_template, make_response
 from flask_restful import Api, Resource
+import requests
 from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin 
 
 import torch
 from network.inference import SimpleGenerator
-from utils import infer
+from utils import infer, cloud_upload
 
 FILE_UPLOAD_TYPES = ["png", 'jpg', "jpeg"]
 
@@ -28,11 +32,31 @@ wgt_path = os.path.join("network", 'weight.pth')
 model.load_state_dict(torch.load(wgt_path, map_location='cpu'))
 model.eval()
 
+def send_file(image, public_id = None, send = False):
+    """
+    Collects a PIL.Image object, converts to byte image and sends it to the cloudinary storage
+    """
+    fileobj = io.BytesIO()
+    image.save(fileobj, format= "PNG", quality = 100)
+    fileobj.seek(0)
+    if send:
+        file_upload = cloud_upload(fileobj, public_id)
+        print(file_upload)
+        return file_upload
+    return fileobj
+
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
+
+
+class Homepage(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
 
 
 class Cartoonize(Resource):
@@ -41,19 +65,27 @@ class Cartoonize(Resource):
         
         image = request.files["image"]
         filename = secure_filename(image.filename)
-        ext = filename.split(".")[-1]
+        name, ext = filename.split(".")[-2:]
+        if len( name ) > 8: name = name[:8]
+        
 
         if ext.lower() not in FILE_UPLOAD_TYPES:
             return Response({"error": "Unsupported file type"}, 200, mimetype="application/json")
 
         try:
+            file = Image.open( image.stream )
+            size = file.size
+            original = send_file(file, name, True)
+            #original = base64.b64encode( file.read() ).decode()
             # Pass image through model for an output
             faceimage = infer(model, image)
-            fileobj = io.BytesIO()
-            faceimage.save(fileobj, format= "PNG", quality = 100)
-            fileobj.seek(0)
+            #faceimage = faceimage.resize(size, Image.LANCZOS)
+            file_link = send_file(faceimage, name + "_Cartoon", True)
+            #fileobj.seek(0)
 
-            return send_file(fileobj, mimetype="image/PNG")
+            
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('result.html', original = original, result = file_link),200,headers)
 
         
         except:
@@ -66,28 +98,38 @@ class GrayCartoonize(Resource):
         
         image = request.files["image"]
         filename = secure_filename(image.filename)
-        ext = filename.split(".")[-1]
+        name, ext = filename.split(".")[-2:]
+        if len( name ) > 8: name = name[:8]
+        print("Image Validated")
 
         if ext.lower() not in FILE_UPLOAD_TYPES:
             return Response({"error": "Unsupported file type"}, 200, mimetype="application/json")
 
         try:
             # Pass image through model for an output and convert to grayscale
+            file = Image.open( image.stream )
+            size = file.size
+            original = send_file(file, name, True)
             faceimage = infer(model, image)
+            print("Transformed")
             cv_img = np.array(faceimage)
+            #faceimage = faceimage.resize(size, Image.LANCZOS)
             cv_img = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
             image = Image.fromarray(cv_img)
-            fileobj = io.BytesIO()
-            image.save(fileobj, format= "PNG", quality = 100)
-            fileobj.seek(0)
+            file_link = send_file(image, name + "_bwcartoon", True)
 
-            return send_file(fileobj, mimetype="image/PNG")
+            
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('result.html', original = original, result = file_link),200,headers)
+
+            #return send_file(fileobj, mimetype="image/PNG")
 
         
         except:
             return Response({"error": "Incompatible image format type"}, 200, mimetype="application/json")
 
-
+routes = ["/", "/home"]
+api.add_resource(Homepage, *routes)
 api.add_resource(Cartoonize, "/cartoonize")
 api.add_resource(GrayCartoonize, "/bwcartoonize")
 
--- Torch-Flask-ImageCartoonization/utils.py
+++ Torch-Flask-ImageCartoonization/utils.py
@@ -3,6 +3,36 @@ from torch.autograd import Variable
 import numpy as np
 from PIL import Image
 
+import os
+import cloudinary
+from dotenv import load_dotenv
+load_dotenv()
+import cloudinary.uploader 
+
+
+
+
+def cloud_upload(file, public_id, resource = "image"):
+    """
+    ### Uploads file by resource type to the Cloudinary cloud server for storage
+    
+
+    file: resource to be uploaded on the Cloudinary server. 
+    resource_type: "image" (default) - Soecifies the type of file to be stored in cloud
+    """
+
+    cloudinary.config( 
+            cloud_name = os.getenv("CLOUD_NAME"), 
+            api_key = os.getenv("API_KEY"), 
+            api_secret = os.getenv("API_SECRET")
+            )
+    print("Configuration Complete")
+
+    upload_result = cloudinary.uploader.upload(file, public_id = public_id, resource_type = resource)
+    print("File Sent")
+
+    return upload_result["secure_url"]
+
 
 
 def infer(model, img_path, load_size = 256):
@@ -17,7 +47,7 @@ def infer(model, img_path, load_size = 256):
 
     if ratio > 1:
         h = load_size
-        w = int( h * 1.0/ w)
+        w = int( h * 1.0/ ratio)
     else:
         w = load_size
         h = int(w * ratio)
@@ -43,7 +73,7 @@ def infer(model, img_path, load_size = 256):
     output = np.clip(output, 0, 255).astype(np.uint8)
     
     # Plot image side by side
-    output = np.concatenate([raw_image, output], axis=1)
+    #output = np.concatenate([raw_image, output], axis=1)
     #cv2.imwrite(save_path, output)
     output = Image.fromarray(output)
 
